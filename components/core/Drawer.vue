366d005379269cfef69b14fee92ec96de7aa2489
burhan ahmed <burhanahmeed@gmail.com>
2020-08-30 10:32:06 +0700

    fix drawer

--- components/core/Drawer.vue
+++ components/core/Drawer.vue
@@ -1,18 +1,18 @@
 <template>
   <c-drawer :isOpen="isOpen" placement="right" :on-close="close">
     <c-drawer-overlay />
-    <c-drawer-content w="28rem">
-    <c-drawer-close-button />
-    <c-drawer-header>{{ title }}</c-drawer-header>
+    <c-drawer-content>
+      <c-drawer-close-button />
+      <c-drawer-header>{{ title }}</c-drawer-header>
 
-    <c-drawer-body>
-      <slot></slot>
-    </c-drawer-body>
+      <c-drawer-body overflow="hidden scroll">
+        <slot></slot>
+      </c-drawer-body>
 
-    <c-drawer-footer>
-      <!-- <c-button variant="outline" mr="3" @click="isOpen = false">Cancel</c-button>
-      <c-button variant-color="blue">Save</c-button> -->
-    </c-drawer-footer>
+      <c-drawer-footer>
+        <!-- <c-button variant="outline" mr="3" @click="isOpen = false">Cancel</c-button>
+        <c-button variant-color="blue">Save</c-button> -->
+      </c-drawer-footer>
     </c-drawer-content>
   </c-drawer>
 </template>
@@ -41,6 +41,10 @@ export default {
   data () {
     return {
     }
+  },
+  mounted () {
+    // var section = document.getElementsByTagName('section')[0];
+    // section.style.maxWidth = '28rem';
   },
   methods: {
     close () {
--- components/home/ChannelDrawer.vue
+++ components/home/ChannelDrawer.vue
@@ -1,24 +1,26 @@
 <template>
-    <drawer :isOpen="isOpen" @close="close" :title="payload.snippet.title" overflow="auto">
-      <c-box align="center">
-        <c-image
-          rounded="full"
-          h="80px"
-          w="80px"
-          :src="payload.snippet.thumbnails.default.url"
-          :alt="payload.snippet.title"
-        />
-      </c-box>
-      <c-box p="4" align="center">
-        <c-text fontSize="xl" fontWeight="700">{{ payload.snippet.title }}</c-text>
-        <c-text fontSize="xs">{{ numberFormat(payload.statistics.subscriberCount) }} subscribers</c-text>
-        <c-badge mr="1" variant-color="green" v-for="lang in payload.lang" :key="lang">{{ lang }}</c-badge>
-      </c-box>
-      <c-box p="4">
-        <c-text fontSize="xs">{{ payload.snippet.description }}</c-text>
-      </c-box>
-      <c-box align="center" mt="4">
-        <c-button variant-color="green" size="xs" as="a" :href="`https://www.youtube.com/channel/${payload.id}`" target="_blank">Visit channel</c-button>
+    <drawer :isOpen="isOpen" @close="close" :title="payload.snippet.title">
+      <c-box>
+        <c-box align="center">
+          <c-image
+            rounded="full"
+            h="80px"
+            w="80px"
+            :src="payload.snippet.thumbnails.default.url"
+            :alt="payload.snippet.title"
+          />
+        </c-box>
+        <c-box p="4" align="center">
+          <c-text fontSize="xl" fontWeight="700">{{ payload.snippet.title }}</c-text>
+          <c-text fontSize="xs">{{ numberFormat(payload.statistics.subscriberCount) }} subscribers</c-text>
+          <c-badge mr="1" variant-color="green" v-for="lang in payload.lang" :key="lang">{{ lang }}</c-badge>
+        </c-box>
+        <c-box p="4">
+          <c-text fontSize="xs">{{ payload.snippet.description }}</c-text>
+        </c-box>
+        <c-box align="center" mt="4">
+          <c-button variant-color="green" size="xs" as="a" :href="`https://www.youtube.com/channel/${payload.id}`" target="_blank">Visit channel</c-button>
+        </c-box>
       </c-box>
     </drawer>
 </template>
